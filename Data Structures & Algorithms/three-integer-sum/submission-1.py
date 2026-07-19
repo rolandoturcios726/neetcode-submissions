1b1b525264bfdd6c669f3c78142f3bbbc9c67907
rolandoturcios726 <rolandoturcios2003@gmail.com>
2026-07-19 18:39:35 -0400

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        solution = set()
+        nums.sort()
+        for i in range(len(nums)-2):
+            complement = -nums[i]
+
+            result = self.twoSum(complement, nums[i+1:])
+            for res in result:
+                solution.add((nums[i],res[0],res[1]))
+        
+        return [list(item) for item in solution]
+
+    def twoSum(self, target, nums: List[int]) -> List[int]:
+        seen = set()
+        solution = set()
+        for num in nums:
+            complement = target - num
+            if complement in seen:
+                solution.add((num, complement))
+            seen.add(num)
+        
+        return [list(item) for item in solution]
